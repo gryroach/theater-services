76a2c41995fd759b5c78fbc8704e304432dece2e
K.Yarkov <k.yarkov@marine-rc.ru>
2024-11-29 16:13:47 +0300

    Task 43: Обернул в backoff

--- theater-async-api/tests/functional/utils/wait_for_es.py
+++ theater-async-api/tests/functional/utils/wait_for_es.py
@@ -1,12 +1,23 @@
-import time
+import logging
 
+import backoff
 from elasticsearch import Elasticsearch
-
 from functional.settings import test_settings
 
+logging.getLogger(__name__)
+
+
+@backoff.on_exception(backoff.expo, ConnectionError, max_tries=10)
+def is_es_available() -> bool:
+    """Проверяет доступность Elasticsearch."""
+    es_client = Elasticsearch(
+        hosts=test_settings.elasticsearch_url, verify_certs=False
+    )
+    if es_client.ping():
+        logging.info("Соединение с ES установленно")
+    else:
+        raise ConnectionError
+
+
 if __name__ == "__main__":
-    es_client = Elasticsearch(hosts=test_settings.elasticsearch_url, verify_certs=False)
-    while True:
-        if es_client.ping():
-            break
-        time.sleep(1)
+    is_es_available()
